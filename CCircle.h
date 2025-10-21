2b5d1b3a042dae9db52d87467402af4d76ff03ca
dimasik <ashikhmin.dima@inbox.ru>
2025-10-21 23:36:21 +0500

    added CCircle logic for painting

--- CCircle.h
+++ CCircle.h
@@ -5,8 +5,13 @@
 #pragma once
 
 #include <QPoint>
+#include <QWidget>
+#include<QPainter>
+#include<QDebug>
 
-class CCircle {
+
+class CCircle : public QWidget {
+    Q_OBJECT
     int x;
     int y;
     int rad = 5;
@@ -15,6 +20,15 @@ public:
     CCircle(const QPoint p) : x(p.x()), y(p.y()){}
 
     void paint_circle(){
+        qDebug()<<"paint_circle\n";
+        update();
+    }
 
+protected:
+    void paintEvent(QPaintEvent *) override {
+        QPainter painter;
+        painter.begin(this);
+        painter.drawEllipse(x,y,2*rad,2*rad);
+        painter.end();
     }
 };
